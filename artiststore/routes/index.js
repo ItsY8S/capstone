b97b04ba391078d5589786fb0e03d86bf4dcbd01
ItsY8S <gri.y8s@gmail.com>
2018-10-06 19:41:25 -0400

    Added Project to dev branch.

---
+++ artiststore/routes/index.js
@@ -0,0 +1,29 @@
+var express = require('express')
+var router = express.Router()
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('dashboard', { title: 'Dashboard' })
+})
+
+router.get('/products', (req, res) => {
+  res.render('products', { title: 'Products' })
+})
+
+router.get('/products/:id', (req, res) => {
+  res.render('product-edit', { title: 'Edit PRODUCT NAME HERE' })
+})
+
+router.get('/orders', (req, res) => {
+  res.render('orders', { title: 'Orders' })
+})
+
+router.get('/concerts', (req, res) => {
+  res.render('concerts', { title: 'Concerts' })
+})
+
+router.get('/manage', (req, res) => {
+  res.render('manage', { title: 'Manage Store' })
+})
+
+module.exports = router
